6814837bcc4433535186b0c9d459619a5a9256c0
tobiastrinks <tobias.trinks@code.berlin>
2018-07-01 21:28:05 +0200

    Fix logging case when log dir/file does not exists

--- logger.js
+++ logger.js
@@ -1,9 +1,21 @@
 const log = require('log-to-file');
+const fs = require('fs');
+const path = require('path');
 
-class Logger {
-  logReq (method, requestedUrl, message) {
-    log(`${method} ${requestedUrl} ${message}`, './logs/node.log');
-  }
-}
+module.exports = class Logger {
+    getLogFile () {
+        const directoryPath = path.join(__dirname, 'logs');
+        const filePath = path.join(directoryPath, 'node.log');
 
-module.exports = Logger;
+        if (!fs.existsSync(directoryPath)) {
+            fs.mkdirSync(directoryPath);
+        }
+        if (!fs.existsSync(filePath)) {
+            fs.writeFileSync(filePath, '');
+        }
+        return filePath;
+    }
+    logReq (method, requestedUrl, message) {
+        log(`${method} ${requestedUrl} ${message}`, this.getLogFile());
+    }
+};
