b0894c35cdb67dbd52713a79066eefc54c7c4a7d
Tobias Trinks <tobias.trinks99@gmx.net>
2018-08-31 16:52:53 +0200

    Fix get active breakpoint by limited given breakpoints

--- assets/js/responsiveHelper.js
+++ assets/js/responsiveHelper.js
@@ -3,8 +3,11 @@ import { breakpoints } from '~/assets/config/styleConfig';
 const breakpointKeys = Object.keys(breakpoints);
 
 export const getActiveBreakpoint = (screenWidth, givenBreakpoints = breakpointKeys) => {
-  for (let x = 0; x < breakpointKeys.length; x++) {
-    let breakpointKey = breakpointKeys.reverse()[x];
+  const breakpointKeysReverse = breakpointKeys.slice();
+  breakpointKeysReverse.reverse();
+
+  for (let x = 0; x < breakpointKeysReverse.length; x++) {
+    let breakpointKey = breakpointKeysReverse[x];
     if (screenWidth >= breakpoints[breakpointKey] && givenBreakpoints.includes(breakpointKey)) {
       return breakpointKey;
     }
