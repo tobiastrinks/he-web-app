13719613d48acc5c4905e5c05258470c60614ed1
Tobias Trinks <tobias.trinks99@gmx.net>
2018-10-25 22:34:53 +0200

    Fix homeOfferMini responsive design

--- components/Home/HomeOfferMini/HomeOfferMini.vue
+++ components/Home/HomeOfferMini/HomeOfferMini.vue
@@ -4,20 +4,27 @@
     :class="{closed: !show}"
   >
     <div class="home-offer-mini-inner">
-      <div class="home-offer-mini-close">
+      <div class="home-offer-mini-close" @click="$emit('close')">
         <FontAwesomeIcon
           :icon="['fas', 'times']"
-          @click="$emit('close')"
         />
       </div>
       <LocaleLink :to="buttonLink">
         <p class="home-offer-mini-headline">
           {{headline}}
         </p>
+        <hr class="hr-short" />
         <p class="home-offer-mini-description">
           {{description}}
         </p>
       </LocaleLink>
+      <div class="home-offer-mini-button">
+        <LocaleLink :to="buttonLink">
+          <ButtonSt>
+            {{button}}
+          </ButtonSt>
+        </LocaleLink>
+      </div>
     </div>
   </div>
 </template>
